403acaf3d33fb724527fdf2e4cf521284236f6d9
Thada Wangthammang <thada.wth@gmail.com>
2026-03-26 23:51:38 +0700

    feat: add initial app structure with routing, theming, and components

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,17 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  root: 'app',
   plugins: [
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths({ root: '..', projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: './routes',
+      generatedRouteTree: './routeTree.gen.ts',
+    }),
     viteReact(),
   ],
 })
